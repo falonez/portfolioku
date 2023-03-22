f4ddb034bcaa5d676d21894b9a2c30108fc3c5fc
Fathan Mubarok <50894040+falonez@users.noreply.github.com>
2023-03-22 23:31:04 +0700

    Update Content Provide

--- components/Provide.vue
+++ components/Provide.vue
@@ -13,51 +13,49 @@
     >
       <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
         <div
-          class="w-full h-[20rem] bg-secondaryLight dark:bg-secondaryDark p-10 flex flex-col justify-between group dark:hover:bg-Light text-myLight dark:text-Light dark:hover:text-myDark"
+          v-for="item in provide"
+          :key="item.id"
+          class="w-full h-[20rem] bg-secondaryLight dark:bg-secondaryDark p-10 flex flex-col justify-between group hover:bg-myLight hover:text-Light dark:hover:bg-Light text-myLight dark:text-Light dark:hover:text-myDark transition-all duration-300 ease-in-out"
         >
-          <h1 class="text-[2.8rem] font-medium leading-none">UI & UX Design</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta,
-            explicabo.
-          </p>
-          <div class="flex flex-row justify-between items-center">
-            <button>Read more</button>
-            <Icon name="uil:arrow-right" />
-          </div>
-        </div>
-        <div
-          class="w-full h-[20rem] bg-secondaryLight dark:bg-secondaryDark p-10 flex flex-col justify-between"
-        >
-          <h1
-            class="text-[2.8rem] font-medium leading-none dark:text-secondaryLight text-slate-700"
-          >
-            UI & UX Design
+          <h1 class="text-[2.8rem] font-medium leading-none">
+            {{ item.title }}
           </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta,
-            explicabo.
-          </p>
-          <div class="flex flex-row justify-between items-center">
+          <p>{{ item.description }}</p>
+          <!-- <div class="flex flex-row justify-between items-center">
             <button>Read more</button>
             <Icon name="uil:arrow-right" />
-          </div>
-        </div>
-        <div
-          class="w-full h-[20rem] bg-slate-200 p-10 flex flex-col justify-between"
-        >
-          <h1 class="text-[2.8rem] font-medium leading-none text-slate-700">
-            UI & UX Design
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta,
-            explicabo.
-          </p>
-          <div class="flex flex-row justify-between items-center">
-            <button>Read more</button>
-            <Icon name="uil:arrow-right" />
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+interface Provide {
+  id: number;
+  title: string;
+  description: string;
+}
+
+const provide: Array<Provide> = [
+  {
+    id: 1,
+    title: "UI & UX Design",
+    description:
+      "Great UI/UX design is about creating seamless and intuitive experiences that help users accomplish their goals.",
+  },
+  {
+    id: 2,
+    title: "Frontend Dev",
+    description:
+      "Frontend development is the art of creating functional and visually appealing interfaces that delight users.",
+  },
+  {
+    id: 3,
+    title: "Backend Dev",
+    description:
+      "Backend development is like the backbone of an application, providing the foundation and functionality that makes it all work seamlessly.",
+  },
+];
+</script>
