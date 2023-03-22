ba8524b1630368c73e3cd3a5d466e400fc3077c8
Fathan Mubarok <50894040+falonez@users.noreply.github.com>
2023-03-22 11:11:47 +0700

    Crete Component Contact

---
+++ components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <section
+    class="flex flex-col justify-center items-center bg-myLight dark:bg-secondaryDark h-1/2 py-10"
+  >
+    <div
+      class="container px-[7%] md:px-[10%] flex flex-col items-center justify-center text-white"
+    >
+      <h1 class="text-2xl mb-3 md:text-3xl md:mb-8">Get in touch</h1>
+      <h2 class="text-4xl mb-3 md:text-5xl">Just Say Hello!</h2>
+      <button
+        class="text-xl dark:bg-myDark dark:hover:bg-Light dark:hover:text-myDark px-4 py-2 my-10"
+      >
+        Contact
+      </button>
+      <div class="text-4xl mb-3 space-x-8">
+        <Icon name="uil:github" />
+        <Icon name="mdi:linkedin" />
+        <Icon name="mdi:instagram" />
+      </div>
+    </div>
+  </section>
+</template>
