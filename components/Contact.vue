9cc966956aed63985bb0fa94501e0325937767f0
Fathan Mubarok <50894040+falonez@users.noreply.github.com>
2023-03-23 14:29:17 +0700

    Update Link and btn cta

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,21 +1,40 @@
 <template>
-  <section
-    class="flex flex-col justify-center items-center bg-myLight dark:bg-secondaryDark h-1/2 py-10"
-  >
-    <div
-      class="container px-[7%] md:px-[10%] flex flex-col items-center justify-center text-white"
-    >
-      <h1 class="text-2xl mb-3 md:text-3xl md:mb-8">Get in touch</h1>
-      <h2 class="text-4xl mb-3 md:text-5xl">Just Say Hello!</h2>
-      <button
-        class="text-xl dark:bg-myDark dark:hover:bg-Light dark:hover:text-myDark px-4 py-2 my-10"
+  <section id="contact" class="pt-10 bg-myLight dark:bg-secondaryDark">
+    <div class="flex flex-col justify-center items-center h-1/2 py-10">
+      <div
+        class="container px-[7%] md:px-[10%] flex flex-col items-center justify-center text-Light"
       >
-        Contact
-      </button>
-      <div class="text-4xl mb-3 space-x-8">
-        <Icon name="uil:github" />
-        <Icon name="mdi:linkedin" />
-        <Icon name="mdi:instagram" />
+        <h1 class="text-2xl mb-3 md:text-3xl md:mb-8">Get in touch</h1>
+        <h2 class="text-4xl mb-3 md:text-5xl">Just Say Hello!</h2>
+        <a
+          class="text-xl bg-secondaryLight text-myLight hover:opacity-50 dark:bg-myDark dark:hover:bg-Light dark:text-Light dark:hover:opacity-100 dark:hover:text-myDark px-4 py-2 my-10"
+          href="mailto:falonez01@gmail.com"
+        >
+          Contact
+        </a>
+        <div class="text-4xl mb-3 space-x-8">
+          <a
+            href="https://github.com/falonez"
+            target="blank"
+            class="hover:opacity-60"
+          >
+            <Icon name="uil:github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/fathan-mubarok-2597171b5/"
+            target="blank"
+            class="hover:opacity-60"
+          >
+            <Icon name="mdi:linkedin" />
+          </a>
+          <a
+            href="https://www.instagram.com/falonez15/"
+            class="hover:opacity-60"
+            target="blank"
+          >
+            <Icon name="mdi:instagram" />
+          </a>
+        </div>
       </div>
     </div>
   </section>
